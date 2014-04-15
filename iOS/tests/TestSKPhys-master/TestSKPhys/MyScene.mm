3e0857e89debd964d1677e3db18ec4b26bc62d31
David Choi <yumyumchoi@gmail.com>
2014-04-14 20:07:58 -0400

    Modified physics test to allow box merging on contact

--- iOS/tests/TestSKPhys-master/TestSKPhys/MyScene.mm
+++ iOS/tests/TestSKPhys-master/TestSKPhys/MyScene.mm
@@ -9,24 +9,26 @@
 #import "MyScene.h"
 #import <Box2D.h>
 
-#define BOX2D
+//#define BOX2D
 #define NUM_BOXES (0)
 #define PIXELS_PER_METER (150)
 #define SMALL_BOX_WIDTH (10)
-#define BIG_BOX_WIDTH (40)
+#define BIG_BOX_WIDTH (50)
 #define SMALL_BOX_HEIGHT (SMALL_BOX_WIDTH / 1)
 #define BIG_BOX_HEIGHT (BIG_BOX_WIDTH / 1)
 #define ANGULAR_VELOCITY (0)
 #define ANGULAR_DAMPING (0)
 #define LINEAR_DAMPING (0)
 #define DENSITY (1.0)
-#define FRICTION (0)
-#define RESTITUTION (0)
-#define GRAVITY (-1)
-#define EPSILON (0)
+#define FRICTION (0.1)
+#define RESTITUTION (0.1)
+#define GRAVITY (-1.5)
+#define EPSILON (0)//.05)
+#define RANDOM_SIZE
+//#define GRID_LAYOUT_FOR_CREATION
 
-float boxWidth = BIG_BOX_WIDTH;//SMALL_BOX_WIDTH;
-float boxHeight = BIG_BOX_HEIGHT;//SMALL_BOX_HEIGHT;
+float boxWidth = BIG_BOX_WIDTH;
+float boxHeight = BIG_BOX_HEIGHT;
 
 @implementation MyScene {
     b2World *world;
@@ -40,6 +42,7 @@ float boxHeight = BIG_BOX_HEIGHT;//SMALL_BOX_HEIGHT;
     double physicsEndTime;
     double simualtionTime;
     double sampleStartTime;
+    
 }
 
 -(id)initWithSize:(CGSize)size {    
@@ -84,10 +87,15 @@ float boxHeight = BIG_BOX_HEIGHT;//SMALL_BOX_HEIGHT;
         boundaryDef.position.Set(0,0);
         b2Body *boundary = world->CreateBody(&boundaryDef);
         boundary->CreateFixture(&chain, 0);
+        
+        // Create contact listener
+//        _contactListener = new MyContactListener();
+//        _world->SetContactListener(_contactListener);
 #else
         NSLog(@"SKPhysics Elastic - %d boxes", NUM_BOXES);
         self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
         self.physicsWorld.gravity = CGVectorMake(0, g);
+        self.physicsWorld.contactDelegate = self;
 #endif
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
@@ -100,26 +108,33 @@ float boxHeight = BIG_BOX_HEIGHT;//SMALL_BOX_HEIGHT;
 -(void)addBoxes {
     float xStart = boxWidth - 3;
     float yStart = self.size.height-2*boxHeight;
-    
+
     int rowSize = self.size.width / (boxWidth + 5);
     
+    CGRect stageRect = [[UIScreen mainScreen] bounds];
+    
     for(int i = 0;i<NUM_BOXES;i++) {
+#ifdef GRID_LAYOUT_FOR_CREATION
         float x = (i % rowSize) * (boxWidth + 5) + xStart;
         float y = yStart - (i / rowSize) * (boxHeight * 1.5);
         [self addBoxAtX:x Y:y];
+#else
+        [self addBoxAtX:arc4random_uniform(stageRect.size.width) Y:arc4random_uniform(stageRect.size.height*.3) + stageRect.size.height*.5 Size:CGSizeMake(boxWidth, boxHeight)];
+#endif
+        
         
     }
 }
 
 
-- (void)addBoxAtX:(float )x Y:(float)y {
-    
-    float newBoxWidth = arc4random_uniform(boxWidth*.8)+ (boxWidth*.2);
-//    CGSize newBoxDimension =  CGSizeMake((arc4random_uniform(newBoxHalf.width)) + newBoxHalf.width, (arc4random_uniform(newBoxHalf.height)) + newBoxHalf.height);
-    
-//    NSLog(@"box dimension = %@",NSStringFromCGSize(newBoxDimension));
-    SKSpriteNode *Box = [[SKSpriteNode alloc] initWithColor:[SKColor whiteColor] size:CGSizeMake(newBoxWidth,newBoxWidth)];
-    
+- (void)addBoxAtX:(float )x Y:(float)y Size:(CGSize)size {
+//#ifdef RANDOM_SIZE
+//    float newBoxWidth = arc4random_uniform(size.width*.8)+ (size.height*.2);
+//    SKSpriteNode *Box = [[SKSpriteNode alloc] initWithColor:[SKColor whiteColor] size:CGSizeMake(newBoxWidth,newBoxWidth)];
+//#else
+    NSLog(@"new size %@",NSStringFromCGSize(size));
+    SKSpriteNode *Box = [[SKSpriteNode alloc] initWithColor:[SKColor whiteColor] size:size];
+//#endif
     Box.position = CGPointMake(x, y);
     
     Box.name = @"Box";
@@ -148,8 +163,8 @@ float boxHeight = BIG_BOX_HEIGHT;//SMALL_BOX_HEIGHT;
     b2PolygonShape polyShape;
         
         
-    
-    polyShape.SetAsBox(Box.size.width / 2.0 / PIXELS_PER_METER, Box.size.height / 2.0 / PIXELS_PER_METER);
+    float gapRemove = 0;//.005f;
+    polyShape.SetAsBox((Box.size.width / 2.0 / PIXELS_PER_METER)-gapRemove, (Box.size.height / 2.0 / PIXELS_PER_METER)-gapRemove);
     fixtureDef.shape = &polyShape;
     
     fixtureDef.density = DENSITY;
@@ -174,7 +189,10 @@ float boxHeight = BIG_BOX_HEIGHT;//SMALL_BOX_HEIGHT;
     Box.physicsBody.density = DENSITY;
     Box.physicsBody.friction = FRICTION;
     Box.physicsBody.linearDamping = LINEAR_DAMPING;
-    
+    Box.physicsBody.categoryBitMask = 10;
+    Box.physicsBody.collisionBitMask = 10;
+    Box.physicsBody.contactTestBitMask = 10;
+    Box.name = @"Box";
 #endif
     
     [self addChild:Box];
@@ -251,7 +269,14 @@ void updatePhysics(double deltaT, double &accumulator, double timeStep, b2World 
     /* Called when a touch begins */
     for (UITouch *touch in touches) {
         CGPoint pos = [touch locationInNode:self];
-        [self addBoxAtX:pos.x Y:pos.y];
+        CGSize size;
+#ifdef RANDOM_SIZE
+        float newBoxWidth = arc4random_uniform(boxWidth*.8)+ (boxWidth*.2);
+        size = CGSizeMake(newBoxWidth, newBoxWidth);
+#else
+        size = CGSizeMake(boxWidth,boxHeight);
+#endif
+        [self addBoxAtX:pos.x Y:pos.y Size:size];
         
     }
 }
@@ -316,5 +341,49 @@ void updatePhysics(double deltaT, double &accumulator, double timeStep, b2World 
 }
 
 #endif
+
+
+- (void)didBeginContact:(SKPhysicsContact *)contact {
+//    SKPhysicsBody *testA = contact.bodyA;
+    SKNode *nodeA = contact.bodyA.node;
+    SKNode *nodeB = contact.bodyB.node;
+    
+    if(nodeA && nodeB){
+        if([nodeA.name isEqualToString:@"Box"] && [nodeB.name isEqualToString:@"Box"]){
+
+            float nodeASurface = nodeA.frame.size.width * nodeA.frame.size.height;//CGSizeMake(nodeA.xScale * nodeA.frame.size.width, nodeA.yScale * nodeA.frame.size.height);
+            float nodeBSurface = nodeB.frame.size.width * nodeB.frame.size.height;//CGSizeMake(nodeB.xScale * nodeB.frame.size.width, nodeB.yScale * nodeB.frame.size.height);
+            float nodeCombinedSurface = sqrtf(nodeASurface + nodeBSurface);
+            CGSize newSize = CGSizeMake(nodeCombinedSurface, nodeCombinedSurface);// CGSizeMake(nodeASize.width + nodeBSize.width, nodeASize.height + nodeBSize.height);
+            
+//            NSLog(@"scale nodeA = %f/%f  nodeB = %f/%f", nodeA.xScale,nodeA.yScale,nodeB.xScale,nodeB.yScale);
+//            NSLog(@"size nodeA = %f/%f  nodeB = %f/%f", nodeA.frame.size.width,nodeA.frame.size.height,nodeB.frame.size.width,nodeB.frame.size.height);
+//            NSLog(@"new size = %@", NSStringFromCGSize(newSize));
+//            NSLog(@"-----");
+            
+            
+            CGPoint newPos;
+//            if(nodeA.position.y < nodeB.position.y){
+            if(nodeASurface > nodeBSurface){
+                newPos = nodeA.position;
+            } else {
+                newPos = nodeB.position;
+            }
+            
+            [self addBoxAtX:newPos.x Y:newPos.y Size:newSize];
+            
+            [nodeA removeFromParent];
+            [nodeB removeFromParent];
+        }
+    }
+//    if(testA){
+//        NSLog(@"test is there!");
+//    }
+//    [nodeA removeFromParent];
+//    [nodeB removeFromParent];
+    //    removeFromParent
+    
+//    NSLog(@"contact");
+}
 
 @end
