bd81c5140cbbedaed3cadcbab465d0d48404c56c
David J Merritt <djmerritt@gmail.com>
2014-04-16 01:36:01 -0400

    Added basic preloading to API test.

--- backend/js/qubitfarm.js
+++ backend/js/qubitfarm.js
@@ -2,7 +2,8 @@
 
 $(document).ready( function() {
 	$('#button').click( function(){
-		$('#returned').css({"opacity":0});
+		$('#button').css({"opacity":0,"display":"none"});
+		$('#returned').text('Loading...');
 		$('#posted').css({"opacity":0});
 		var thePost = $('#post').val();
 		$('#posted').animate({"opacity":1},250);
@@ -20,10 +21,12 @@ function getData(thePost) {
 		dataType: 'json',
 		data: thePost,
 		success: function(data) {
+			$('#button').css({"opacity":1,"display":"block"});
 			$('#returned').animate({"opacity":1},250);
 			$('#returned').html('<b>Return (Success) // '+new Date()+'</b><pre>'+JSON.stringify(data, undefined, 2)+'</pre>');
 		},
 		error: function(data) {
+			$('#button').css({"opacity":1,"display":"block"});
 			$('#returned').animate({"opacity":1},250);
 			$('#returned').html('<b>Return (Error) // '+new Date()+'</b><pre>'+JSON.stringify(data, undefined, 2)+'</pre>');
 		}
